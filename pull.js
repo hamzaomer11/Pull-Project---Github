2f9483bf0545b60c1f89508692de8e102e6f997b
hamesomer11 <hamzaomer45@gmail.com>
2023-09-18 23:12:29 +0100

    fresh update

--- pull.js
+++ pull.js
@@ -1,3 +1,17 @@
+const query_search = document.getElementById('search').value
 
+const access_token = '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'
+const final_url = `https://api.github.com/users/${query_search}`
 
- 
+async function getUser() {
+    await fetch(final_url, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': `token ${access_token}`,
+        }
+    })
+    .then((res) => res.json())
+    .then((data) => 
+    document.getElementById('note').innerHTML = (data))
+}
