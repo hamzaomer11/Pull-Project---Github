fddc332d9e2462d4b7e7de5508a035c8196eb9bb
hamesomer11 <hamzaomer45@gmail.com>
2023-09-19 22:03:19 +0100

    fresh commit

--- pull.js
+++ pull.js
@@ -1,17 +1,26 @@
-const query_search = document.getElementById('search').value
-
-const access_token = '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'
-const final_url = `https://api.github.com/users/${query_search}`
-
 async function getUser() {
+    const query_search = document.getElementById('search').value
+
+    const access_token = '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'
+    const final_url = `https://api.github.com/users/${query_search}`;
     await fetch(final_url, {
         method: 'GET',
         headers: {
             'Accept': 'application/json',
-            'Authorization': `token ${access_token}`,
+            'Authorization': `Bearer ${access_token}`
         }
     })
     .then((res) => res.json())
     .then((data) => 
-    document.getElementById('note').innerHTML = (data))
+    document.getElementById('note').innerHTML = (JSON.stringify(data, null, 2)))
+    .catch ((error) => {
+        console.error('Error:', error);
+        document.getElementById('note').innerHTML = 'An error occurred.';
+    })
 }
+
+function scrollSection() {
+    document.getElementById("scroll-container").style.overflow = "scroll";
+}
+
+
